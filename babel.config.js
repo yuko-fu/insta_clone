96bee79bad8d60d73279345e21105cbe08c239f4
yuko <ryoyu-fu@live.jp>
2023-06-13 11:01:21 +0900

    fix babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -76,7 +76,8 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      ["@babel/plugin-proposal-private-methods", { "loose": true }]
     ].filter(Boolean)
   }
 }
