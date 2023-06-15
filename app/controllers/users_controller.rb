54cc1dbc9ef34892837f95b83e30f93be1912728
yuko <ryoyu-fu@live.jp>
2023-06-15 13:25:31 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to pictures_path
+    end
   end 
 
 
